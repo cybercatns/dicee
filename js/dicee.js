f22b277bfbbb478d075ffa5e997e1b4f3d30ebf9
cybercatns <contactsahan@gmail.com>
2020-10-26 01:26:18 +0530

    updated to jquery

--- js/dicee.js
+++ js/dicee.js
@@ -1,5 +1,3 @@
-console.log("js loaded")
-
 var dc = 6;
 var p1Num = p1rGen(dc);
 var p2Num = p2rGen(dc);
@@ -9,27 +7,30 @@ winWin(p1Num, p2Num);
 function p1rGen(dc) {
     var pr1gen = Math.floor(Math.random() * Math.floor(dc)) + 1;
     console.log("Player1 dice: "+pr1gen);
-    document.querySelectorAll("img")[0].src="images/dice"+pr1gen+".png";
+    var pr1genhtml = "images/dice"+pr1gen+".png"
+    console.log(pr1genhtml);
+    $("#p1Img").attr("src", pr1genhtml);
     return pr1gen;     
 }
 
 function p2rGen(dc) {
     var pr2gen = Math.floor(Math.random() * Math.floor(dc)) + 1;
     console.log("Player2 dice: "+pr2gen);
-    document.querySelectorAll("img")[1].src="images/dice"+pr2gen+".png";
+    var pr2genhtml = "images/dice"+pr2gen+".png"
+    console.log(pr2genhtml);
+    $("#p2Img").attr("src", pr2genhtml);
     return pr2gen; 
 }
 
 function winWin(p1Num, p2Num){
     if(p1Num > p2Num){
         console.log("Winner is player 1 " );
-        document.querySelector("h1").innerHTML="<ion-icon name='flag'></ion-icon> Player 1 Wins!";
-        // "<ion-icon name='flag-outline'></ion-icon>Player 1 Wins!"
+        $("h1").html("<ion-icon name='flag'></ion-icon> Player 1 Wins!");
     }else if(p1Num == p2Num){
         console.log("Game is draw " );
-        document.querySelector("h1").innerText="Draw! Try Again. ";
+        $("h1").text("Draw! Try Again. ");
     }else{
         console.log("Winner is player 2 " );
-        document.querySelector("h1").innerHTML="Player 2 Wins! <ion-icon name='flag'></ion-icon>";
+        $("h1").html("Player 2 Wins! <ion-icon name='flag'></ion-icon>");
     }
 }
